2a228345ab3ba8ab65e799b40e0174df856cc0d3
mtl1015 <95431754+mtl1015@users.noreply.github.com>
2022-07-16 16:01:06 +0700

    more dedicated router and controller

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { watch, edit } from "../controllers/videoController.js";
+import { see, edit, upload, deleteVideo } from "../controllers/videoController.js";
 
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watch);
-videoRouter.get("/edit", edit);
+videoRouter.get("/upload", upload);
+videoRouter.get("/:id", see);
+videoRouter.get("/:id/edit", edit);
+videoRouter.get("/:id/delete", deleteVideo);
+
 
 
 export default videoRouter;
