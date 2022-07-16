cfa612dc2ba3ddc3632e66b724dd9dd553565979
mtl1015 <95431754+mtl1015@users.noreply.github.com>
2022-07-16 15:37:37 +0700

    ++controller

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { watch, edit } from "../controllers/videoController.js";
+
 
 const videoRouter = express.Router();
 
-const handleWatchVideo = (req,res) => res.send("Watch Video.");
+videoRouter.get("/watch", watch);
+videoRouter.get("/edit", edit);
 
-videoRouter.get("/watch", handleWatchVideo);
 
 export default videoRouter;
