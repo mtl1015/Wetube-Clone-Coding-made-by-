549b3afa01177d865f9242f097f3251f0fc127a2
mtl1015 <95431754+mtl1015@users.noreply.github.com>
2022-07-16 15:20:30 +0700

    2022-07-16
    restart

--- src/server.js
+++ src/server.js
@@ -1,11 +1,24 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter.js";
+import userRouter from "./routers/userRouter.js";
+import videoRouter from "./routers/videoRouter.js";
 
 const PORT = 3000;
+const logger = morgan("dev");
 
-const app = express();
+const app = express();//application 생성
+
+
+app.use("/",globalRouter);
+app.use("/users",userRouter);
+app.use("/videos",videoRouter);
+
+ 
+
 
 const handleListening = () => {
-    console.log(`서버가 잘 작동되고 있음`);
+    console.log(`서버 ${PORT}이 잘 작동되고 있음`);
 }
 
 app.listen(PORT, handleListening);
